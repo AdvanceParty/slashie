4ebecc3090ee85fa61af68246766f7c819d389a6
Gil Fewster <hello@gilfewster.com>
2020-01-21 15:37:16 +1100

    Parsing request body for content

--- api/foo.js
+++ api/foo.js
@@ -1,4 +1,14 @@
+const { WebClient } = require('@slack/web-api');
+const token = process.env.AU_COM_ADVANCEPARTY_SLASHIE_TOKEN;
+const web = new WebClient(token);
+
+const MESSAGES = {
+  NO_REQUEST: 'You need to ask me to do something.',
+  REQUEST_NOT_RECOGNISED: "I don't know how to do that."
+};
+
 module.exports = (req, res) => {
-  const message = 'bar';
-  res.status(200).send(message);
+  const { body } = req;
+  const replyMessage = body ? body : MESSAGES.NO_REQUEST;
+  res.status(200).send(replyMessage);
 };
